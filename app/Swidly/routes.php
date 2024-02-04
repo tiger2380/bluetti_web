c7704f46f20df1be67c9335bdd6168c28631cba2
Thaddeus Bibbs <thaddeus.bibbs@hotmail.com>
2024-02-03 22:45:02 -0600

    updates

--- app/Swidly/routes.php
+++ app/Swidly/routes.php
@@ -27,12 +27,8 @@ $this->get('/sw_admin/themes', function($req, $res) {
 
 $this->post('/api/monitoring', function($req, $res) {
     $data = $req->getDecoded();
-
-    $body = [];
+    $data['ip'] = $_SERVER['REMOTE_ADDR'];
     $sql = "INSERT INTO `monitoring`(`data`, `timestamp`, `user_id`) VALUES (?, ?, ?)";
-    foreach ($data as $key => $value) {
-        $body[] = "JSON_OBJECT('$key', '$value')";
-    }
 
     $payload = [
         json_encode($data, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES),
